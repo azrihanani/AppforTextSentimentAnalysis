cf8791c93b935763fa7527faf50b0f3af8a19297
azrihanani <148434413+azrihanani@users.noreply.github.com>
2024-02-22 17:31:24 +0800

    Update mainbarpage.py

--- mainbarpage.py
+++ mainbarpage.py
@@ -2,7 +2,6 @@ import streamlit as st
 import sidebarpage
 import textblob
 page=sidebarpage.show()
-import textbolbalgo
-  if(page=="TextBlob Analysis"):
+if(page=="TextBlob Analysis"):
      textblobalgo.displayPage()
 
